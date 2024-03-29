9648a5971737e9b566e2767c9c5f604ae0f0b3a5
Sumit Dey <sumit@betterhalf.ai>
2024-03-30 01:58:37 +0530

    created boilerplate using tailwind mantine next typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@mantine/core/styles.css";
+// all the other mantine style should go here, after core and before global css
+
+// import "@mantine/dates/styles.css";
+// import "@mantine/dropzone/styles.css";
+
+import "@/styles/globals.css";
+import { theme } from "@/theme";
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <title>Mantine Template</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
